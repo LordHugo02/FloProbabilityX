d473276567ff7620ba7dc688043df11664ba908a
FLEJOSNE <florian.lejosne@ynov.com>
2024-02-10 18:59:52 +0100

    [ADD] scrapping pages

--- ProbabilityX_API/Services/EarningCalendarService.cs
+++ ProbabilityX_API/Services/EarningCalendarService.cs
@@ -5,6 +5,8 @@ using ProbabilityX_API.Models;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using Microsoft.Extensions.Options;
+using Microsoft.CodeAnalysis.Text;
 
 namespace ProbabilityX_API.Services
 {
@@ -21,6 +23,7 @@ namespace ProbabilityX_API.Services
         {
             var chromeOptions = new ChromeOptions();
             //chromeOptions.AddArgument("--headless"); // Run Chrome in headless mode (without UI)
+            chromeOptions.AddArguments("--ignore-certificate-errors");
             var driver = new ChromeDriver(chromeOptions);
 
             try
@@ -31,11 +34,85 @@ namespace ProbabilityX_API.Services
                 var acceptButtonCookie = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
                 acceptButtonCookie.Click();
                 var closeButtonRegister = WaitUntilElementIsVisibleWithRetry(driver, By.ClassName("popupCloseIcon"), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(1));
-                closeButtonRegister.Click();
+                if (closeButtonRegister != null)
+                {
+                    closeButtonRegister.Click();
+                }
                 var filterNexWeek = driver.FindElement(By.Id("timeFrame_nextWeek"));
                 filterNexWeek.Click();
-                var date = driver.FindElement(By.ClassName("theDay"));
-                Console.WriteLine(date);
+              
+                for (int i = 0; i < 3; i++)
+                {
+                    IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                    js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+                    await Task.Delay(2000);
+                }
+
+                var links = driver.FindElements(By.CssSelector("a.bold.middle"));
+
+                foreach (var link in links)
+                {
+                    var principalHandle = driver.CurrentWindowHandle;
+
+                    // Utiliser JavaScript pour cliquer sur le lien
+                    IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+                    executor.ExecuteScript("arguments[0].click();", link);
+
+                    // Attendez que la nouvelle fenêtre soit disponible
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                    wait.Until(driver => driver.WindowHandles.Count > 1);
+
+                    // Basculer vers la nouvelle fenêtre
+                    var newHandle = driver.WindowHandles.Except(new List<string> { principalHandle }).Single();
+                    driver.SwitchTo().Window(newHandle);
+
+                    try
+                    {
+                        // Récupérez la donnée souhaitée ici
+                        var yourDataElement = driver.FindElement(By.Id("yourDataElementId"));
+                        string yourData = yourDataElement.Text;
+                        Console.WriteLine($"Donnée récupérée : {yourData}");
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        Console.WriteLine("L'élément n'a pas été trouvé.");
+                    }
+                    finally
+                    {
+                        // Fermez la nouvelle fenêtre
+                        driver.Close();
+
+                        // Revenez à la fenêtre principale
+                        driver.SwitchTo().Window(principalHandle);
+                    }
+                }
+
+                //// Attendre un moment pour permettre le chargement des dates supplémentaires
+                //var dateElements = driver.FindElements(By.ClassName("theDay"));
+                //Console.WriteLine(dateElements.Count);
+                //var companyNames = driver.FindElements(By.ClassName("earnCalCompanyName"));
+                //Console.WriteLine(companyNames.Count);
+                //var companyTags = driver.FindElements(By.CssSelector(".bold.middle"));
+                //Console.WriteLine(companyTags.Count);
+                //var epsElements = driver.FindElements(By.CssSelector("[class*='eps_actual']"));
+                //Console.WriteLine(epsElements.Count);
+
+                //string filePath = "fichier.txt";
+
+                //// Utilisez StreamWriter pour écrire dans le fichier
+                //using (StreamWriter sw = new StreamWriter(filePath))
+                //{
+                //    for (int i = 0; i < companyNames.Count; i++)
+                //    {
+                //        string companyNameText = companyNames[i].Text;
+                //        string companyTagText = companyTags[i].Text;
+                //        string epsText = epsElements[i].Text;
+
+                //        // Écrire les données dans le fichier
+                //        sw.WriteLine($"Company Name: {companyNameText}, Company Tag: {companyTagText}, EPS: {epsText}");
+                //    }
+                //}
+
             }
             catch (Exception ex)
             {
@@ -53,15 +130,10 @@ namespace ProbabilityX_API.Services
         }
 
         // Fonction pour attendre qu'un élément soit visible
-        IWebElement WaitUntilElementIsVisible(IWebDriver driver, By by, TimeSpan timeout)
-        {
-            var wait = new WebDriverWait(driver, timeout);
-            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
-        }
-
         IWebElement WaitUntilElementIsVisibleWithRetry(IWebDriver driver, By by, TimeSpan timeout, TimeSpan retryInterval)
         {
             var wait = new WebDriverWait(driver, timeout);
+            var retryNumber = 0;
             IWebElement element = null;
             try
             {
@@ -73,11 +145,13 @@ namespace ProbabilityX_API.Services
             }
 
             // Si l'élément n'est pas encore visible, réessayer avec un intervalle
-            while (element == null)
+            while (element == null || retryNumber < 5)
             {
                 Task.Delay(retryInterval).Wait();
                 try
                 {
+                    Console.WriteLine("Retry", retryNumber);
+                    retryNumber= retryNumber + 1;
                     element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
                 }
                 catch (WebDriverTimeoutException)
@@ -87,13 +161,6 @@ namespace ProbabilityX_API.Services
             }
 
             return element;
-        }
-
-        // Fonction pour attendre qu'un élément soit cliquable
-        IWebElement WaitUntilElementIsClickable(IWebDriver driver, By by, TimeSpan timeout)
-        {
-            var wait = new WebDriverWait(driver, timeout);
-            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
         }
     }
 }
