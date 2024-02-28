b1669066f913f46f218821635bbb55bd9f7f9753
Florian LEJOSNE <florian.lejosne@byg4lab.com>
2024-02-28 12:27:24 +0100

    [ADD] top movement

---
+++ 02_Models/TopMovementOfTheDayModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProbabilityX_API.Models
+{
+    public class TopMovementOfTheDay
+    {
+        [Key]
+        public int Id_TopMovementOfTheDay { get; set; }
+        public int Id_Company { get; set; }
+        public int Movement_Pourcentage_Start_Of_Day { get; set; }
+        public int Movement_Pourcentage_End_Of_Day { get; set;}
+        public int Movement_Pourcentage_Benefice_Possible { get; set; }
+    }
+}
